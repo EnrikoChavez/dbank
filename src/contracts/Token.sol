5dc36b8b9de14dd442a9647f6deaf7ada052f0fa
Enriko Chavez <enriko@mit.edu>
2022-01-09 14:21:58 -0500

    created basic ERC-20 token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,21 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
+  constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+  function passMinterRole(address dBank) public returns (bool) {
+    require(msg.sender == minter, "Error, msg.sender is not the minter of the token");
+    minter = dBank;
+
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
-		_mint(account, amount);
+		require(msg.sender == minter, "Error, msg.sender is not the minter of the token");
+    _mint(account, amount);
 	}
 }
